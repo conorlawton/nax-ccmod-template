5c1215981f8666c69ee0c7abd62fe4e285c8e8fa
naxane <nax@vivaldi.net>
2024-08-27 15:39:21 +0100

    set destDir to obvious warning

--- esbuild-post.mjs
+++ esbuild-post.mjs
@@ -6,7 +6,7 @@ export default function post() {
 	const __filename = url.fileURLToPath(import.meta.url);
 	const __dirname = path.dirname(__filename);
 	const modname = path.basename(__dirname);
-	const destDir = "D:/Programming/CrossCodeModding/CrossCode CCL2 nwjs 0.35.5 dev/assets/mods/";
+	const destDir = "YOU SHOULD CHANGE THIS TO BE WHERE YOUR MODS FOLDER IS: blah/CrossCode/assets/mods";
 	const from = path.join(__dirname, `dist/${modname}.ccmod`);
 	const to = path.join(destDir, `${modname}.ccmod`);
 	fs.copyFileSync(from, to);
